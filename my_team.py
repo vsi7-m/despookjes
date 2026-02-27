8ede2a85d887b777bcc982fb35eb73e4bc20d2af
vsi7-m <lisawei2711@gmail.com>
2026-02-27 11:51:46 +0100

    Offensive agent extra features gegeven en weights aangepast

--- my_team.py
+++ my_team.py
@@ -70,6 +70,49 @@ class ReflexCaptureAgent(CaptureAgent):
         self.start = game_state.get_agent_position(self.index)
         CaptureAgent.register_initial_state(self, game_state)
 
+        # Berekenen wat een tunnel is van tevoren omdat we maar 1 seconde compute time hebben
+        # "Is Pacman in a tunnel" 
+        self.dead_end_tunnels = {}
+
+        walls = game_state.get_walls()
+        width, height = walls.width, walls.height
+
+        def get_free_neighbours(pos):
+            x, y = pos
+            neighbours = [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]
+            return [pos for pos in neighbours if not game_state.has_wall(pos[0], pos[1])]
+
+        dead_ends = []
+        for x in range(width):
+            for y in range(height):
+                if not game_state.has_wall(x, y):
+                    neighbours = get_free_neighbours((x, y))
+                    if len(neighbours) == 1:
+                        dead_ends.append((x, y))
+        
+        for end in dead_ends:
+            curr = end
+            prev = None
+            depth = 1 # lengte van de tunnel
+            tunnel_coords = []
+        
+            while True:
+                neighbours = get_free_neighbours(curr)
+                if len(neighbours) > 2: 
+                    entrance = curr
+                    break # om uit de while loop te gaan
+
+                tunnel_coords.append(curr) # coördinaten van de tunnel
+                next_coord = [coord for coord in neighbours if coord != prev][0]
+                prev = curr
+                curr = next_coord
+                depth += 1
+
+            for coord, depth in tunnel_coords:
+                self.dead_end_tunnels[coord] = entrance
+
+
+
     def choose_action(self, game_state):
         """
         Picks among the actions with the highest Q(s,a).
@@ -107,7 +150,7 @@ class ReflexCaptureAgent(CaptureAgent):
         successor = game_state.generate_successor(self.index, action)
         pos = successor.get_agent_state(self.index).get_position()
         if pos != nearest_point(pos):
-            # Only half a grid position was covered     # Hoe kan dit???? Accepteren => Arno wist het zelf niet maar is vgm voor edge case dus boeie
+            # Only half a grid position was covered
             return successor.generate_successor(self.index, action)
         else:
             return successor
@@ -151,15 +194,26 @@ class OffensiveReflexAgent(ReflexCaptureAgent): # inspiratie van de slides Appro
         food_list = self.get_food(successor).as_list()
         capsule_list = self.get_capsules(successor)
         opponents_list = [successor.get_agent_state(opponent) for opponent in self.get_opponents(successor)]
-        ghost_within_5_list = [ghost for ghost in opponents_list if (not ghost.is_pacman) and (ghost.get_position() is not None)] # Gelijkaardige code als bij Defensive
+        ghost_within_5_list = [
+            ghost 
+            for ghost in opponents_list
+            if (not ghost.is_pacman) and
+            ghost.get_position() is not None and 
+            ghost.scared_timer == 0
+        ] # Misschien beter als we zo typen wanneer tekst niet meer op het scherm past?
+        scared_ghosts_list = [
+            ghost 
+            for ghost in opponents_list
+            if (not ghost.is_pacman) and
+            ghost.get_position() is not None and 
+            ghost.scared_timer > 0
+        ]
 
         agent = successor.get_agent_state(self.index)
         my_pos = agent.get_position()
-        
 
         # Om food op te eten
         features['remaining_food'] = len(food_list)  # self.get_score(successor)
-
 
         width = game_state.data.layout.width
         height = game_state.data.layout.height
@@ -167,11 +221,10 @@ class OffensiveReflexAgent(ReflexCaptureAgent): # inspiratie van de slides Appro
         if self.red:
             middle = middle - 1 # middle is het einde van ons eigen territorium, dus waar pacman terug een spookje wordt
         #else: middle = middle + 1 
-        # Niet 100% zeker dat de berekening van middle helemaal klopt maar denk dat het goed is
+        # Berekening klopt denk ik wel
         middle_positions = [(middle, h) for h in range(1, height) if not game_state.has_wall(middle, h)] # Als je maze distance berekent met een positie waar een wall is wordt blijkbaar een error gethrowd
         distances_to_home = [self.get_maze_distance(my_pos, pos) for pos in middle_positions] 
         features['distance_to_home'] = min(distances_to_home) # Kortste pad naar positie waar we weer ghost worden
-
 
         # Compute distance to the nearest food
         if len(food_list) > 0:  # This should always be True,  but better safe than sorry
@@ -186,11 +239,35 @@ class OffensiveReflexAgent(ReflexCaptureAgent): # inspiratie van de slides Appro
         # We moeten dan ook reward geven om een capsule te eten, anders wordt na het eten de afstand tot een andere capsule plots kei groot waardoor pacman de capsule niet wilt eten
         # => Dit blijft een probleem, wrs lukt het wel met de juiste weights maar moeilijk om te vinden
         features['remaining_capsules'] = len(capsule_list)
+
+        # Doodgaan is heel slechttt (dat wist pacman niet als hij zat in de tunnel, hij begon daar gewoon te chillen)
+        current_pos = game_state.get_agent_state(self.index).get_position()
+        features['death'] = 0
+        if my_pos == self.start and current_pos != self.start:
+            features['death'] = 1
         
         # Compute distance to the nearest ghost within Manhattan distance 5
-        if len(ghost_within_5_list) > 0 and ghost_within_5_list[0].scared_timer == 0: # Als de ghosts scared zijn hoeven we er niet van weg te gaan (maar mss als de timer net gedaan gaat zijn wel, kunnen we nog verbeteren)
+        # Als de ghosts scared zijn, hoeven we er niet van weg te gaan (maar mss als de timer net gedaan gaat zijn wel, kunnen we nog verbeteren)
+        if len(ghost_within_5_list) > 0 and features['death'] == 0: # Weglopen is alleen belangrijk als we levend kunnen blijven (en dus niet vastzitten in een tunnel)
             min_ghost_distance = min([self.get_maze_distance(my_pos, ghost.get_position()) for ghost in ghost_within_5_list])
-            features['distance_to_ghost'] = min_ghost_distance
+            features['distance_to_ghost'] = 1 / min_ghost_distance  # 1/ want pacman bij die positieve reward/weight probeerde pacman die 5 distance te houden, 1/ is dus beter met negatieve reward/weight
+
+        # Compute distance to the nearest scared ghost
+        if len(scared_ghosts_list) > 0:
+            min_scared_ghost_distance = min([self.get_maze_distance(my_pos, ghost.get_position()) for ghost in scared_ghosts_list])
+            closest_scared_ghost = [ghost for ghost in scared_ghosts_list if self.get_maze_distance(my_pos, ghost.get_position()) == min_scared_ghost_distance][0]
+            if closest_scared_ghost.scared_timer > min_scared_ghost_distance + 2: # Genoeg tijd geven om de ghost te eten
+                features['distance_to_scared_ghost'] = min_scared_ghost_distance
+            else:
+                features['distance_to_ghost'] = max(features['distance_to_ghost'], 1 / min_scared_ghost_distance) # sws wegrennen van gewone ghost of scared ghost die bijna niet scared gaat zijn
+        
+        features['ate_scared_ghost'] = 0 # toegevoegd omdat hij scared ghost niet wilde opeten tot het laatste moment
+        current_opponents = [game_state.get_agent_state(opponent) for opponent in self.get_opponents(game_state)]
+        for ghost in current_opponents:
+            if not ghost.is_pacman and ghost.get_position() is not None and ghost.scared_timer > 0:
+                if my_pos == ghost.get_position():
+                    features['ate_scared_ghost'] = 1
+
         
             
         # Nu alleen nog maar de distance binnen die 5 geïmplementeerd, maar daarbuiten moeten we de noisy distance 
@@ -198,9 +275,34 @@ class OffensiveReflexAgent(ReflexCaptureAgent): # inspiratie van de slides Appro
         # want die houdt geen rekening met muren dus idk hoe ik verder moest
         # => Op zich als de ghost meer dan 5 verwijderd is hoeven we er ons helemaal geen zorgen over te maken, pas 
         # als die dichter komt moeten we weglopen. Dus mss hoeven we vr de offensive agent die noisy distance niet eens te gebruiken.
+        # Vind ik goeddd!!!!
 
-        # Hier kan ook de feature van de slides erbij van "is Pacman in a tunnel" 0 of 1 aangeven of de actie pacman zal trappen 
-        
+        # "is Pacman in a tunnel" 0 of 1 aangeven of de actie pacman zal trappen 
+        features["dead_end_tunnel"] = 0
+
+        if my_pos in self.dead_end_tunnels:
+            entrance = self.dead_end_tunnels[my_pos]
+
+            pacman_dist_to_entrance = self.get_maze_distance(my_pos, entrance)
+            current_dist_to_entrance = self.get_maze_distance(current_pos, entrance)
+            escaping = pacman_dist_to_entrance < current_dist_to_entrance
+            
+            for opponent in opponents_list:
+                if opponent.is_pacman or opponent.scared_timer > 0:
+                    continue # We gaan verder met de volgende opponent in de list want deze opponent kan ons niet aanvallen
+
+                ghost_pos = opponent.get_position() # Is de ghost dichtbij genoeg?
+                if ghost_pos is None: # Als het None is, weten we dat de ghost niet 5 distance is, anders heeft die een coördinaat
+                    continue # We gaan verder met de volgende opponent in de list want deze opponent is te ver
+                
+                ghost_dist_to_entrance = self.get_maze_distance(ghost_pos, entrance)
+                
+                if ghost_dist_to_entrance <= pacman_dist_to_entrance + 3 and not escaping: # + 3 zodat pacman niet een tunnel ingaat wanneer een ghost hem achtervolgt (we kunnen ipv +3 de depth gebruiken om de buffer preciezer te berekenen)
+                    features["dead_end_tunnel"] = 1
+                    break # Als 1 ghost ons gaat killen in de tunnel, zijn we dood en is de loop klaar
+
+        # We moeten sws nog een feature toepassen dat onze attacker rondgaat als hij heel de tijd de enemy tegenkomt langs dezelfde kant
+
         return features
 
     def get_weights(self, game_state, action):
@@ -215,8 +317,12 @@ class OffensiveReflexAgent(ReflexCaptureAgent): # inspiratie van de slides Appro
                 'distance_to_home': distance_to_home_weight, # Probeer naar huis te gaan wnr je veel food draagt
                 #'distance_to_capsule': -2, # Ik krijg het niet echt goed met die capsules, plots staat hij vaak stil enzo.
                 #'remaining_capsules': -30, # Dus mss beter voor later houden en eerst gwn simpel beginnen zonder capsules
-                'distance_to_ghost': 100} # Verhoog afstand met ghost die maar op afstand van 5 of minder is
-    # De weights kunnen wrs nog veel beter
+                'distance_to_ghost': -100, # Hoe dichter de ghost van die 5 distance, hoe negatiever de q-value 
+                'distance_to_scared_ghost': -8, # Zodat als hij de kans krijgt, dat hij die wel echt neemt
+                'ate_scared_ghost': 500,
+                'death': -10000, 
+                'dead_end_tunnel': -10000} # Nu weet hij zeker dat dit een slechte state is
+    # De weights kunnen wrs nog veel beter !
 
 
 class DefensiveReflexAgent(ReflexCaptureAgent):
@@ -257,14 +363,15 @@ class DefensiveReflexAgent(ReflexCaptureAgent):
         # Ik heb deze getest en heb het gevoel dat da echt nog goe kan zijn.
         # Ook voor een meer dynamische agent is da mss goed want dan is hij al dicht bij het territorium van de tegenstander
         # en kan hij af en toe food gaan stelen mss
-        width = game_state.data.layout.width
-        height = game_state.data.layout.height
+        walls = game_state.get_walls()
+        width = walls.width
+        height = walls.height
         middle = width // 2
         if self.red:
             middle = middle - 1 # middle is het einde van ons eigen territorium, dus waar pacman terug een spookje wordt
         #else: middle = middle + 1 
         # Niet 100% zeker dat de berekening van middle helemaal klopt maar denk dat het goed is
-        middle_positions = [(middle, h) for h in range(1, height) if not game_state.has_wall(middle, h)] # Als je maze distance berekent met een positie waar een wall is wordt blijkbaar een error gethrowd
+        middle_positions = [(middle, h) for h in range(height) if not game_state.has_wall(middle, h)] # Als je maze distance berekent met een positie waar een wall is wordt blijkbaar een error gethrowd
         # number_of_entry_points = len(middle_positions)   
         distances_to_home = [self.get_maze_distance(my_pos, pos) for pos in middle_positions] 
         features['distances_to_home'] = sum(distances_to_home) # / number_of_entry_points 
